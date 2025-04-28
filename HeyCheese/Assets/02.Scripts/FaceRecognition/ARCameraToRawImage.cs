17c9861c278001df4e60448eec0e2c02e2b23777
limce106 <limce106@naver.com>
2025-04-29 00:48:54 +0900

    Fix: AR Camera To Raw Image Rotation
    - AR Camera와 Raw Image 회전 값 돌아가는 문제 해결 완료
    
    - Raw Image 크기는 4:3, 1000 x 750

--- HeyCheese/Assets/02.Scripts/FaceRecognition/ARCameraToRawImage.cs
+++ HeyCheese/Assets/02.Scripts/FaceRecognition/ARCameraToRawImage.cs
@@ -1,7 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 [RequireComponent(typeof(ARCameraManager))]
 public class ARCameraToRawImage : MonoBehaviour
@@ -10,6 +12,9 @@ public class ARCameraToRawImage : MonoBehaviour
 
     private ARCameraManager cameraManager;
     private Texture2D cameraTexture;
+    private NativeArray<byte> rawPixelBuffer;
+    private NativeArray<Color32> colorPixelBuffer;
+    private int bufferSize;
 
     void Awake()
     {
@@ -26,6 +31,37 @@ public class ARCameraToRawImage : MonoBehaviour
     {
         if (cameraManager != null)
             cameraManager.frameReceived -= OnCameraFrameReceived;
+
+        if (rawPixelBuffer.IsCreated)
+            rawPixelBuffer.Dispose();
+        if (colorPixelBuffer.IsCreated)
+            colorPixelBuffer.Dispose();
+    }
+
+    private Texture2D Rotate90(Texture2D originalTexture)
+    {
+        int width = originalTexture.width;
+        int height = originalTexture.height;
+        Texture2D rotatedTexture = new Texture2D(height, width, originalTexture.format, false);
+
+        NativeArray<Color32> originalPixels = originalTexture.GetRawTextureData<Color32>();
+        NativeArray<Color32> rotatedPixels = rotatedTexture.GetRawTextureData<Color32>();
+
+        int originalStride = width;
+        int rotatedStride = height;
+
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                rotatedPixels[x * rotatedStride + (rotatedStride - 1 - y)] = originalPixels[y * originalStride + x];
+            }
+        }
+
+        rotatedTexture.Apply();
+        originalPixels.Dispose();
+        rotatedPixels.Dispose();
+        return rotatedTexture;
     }
 
     private void OnCameraFrameReceived(ARCameraFrameEventArgs args)
@@ -43,21 +79,38 @@ public class ARCameraToRawImage : MonoBehaviour
                 transformation = XRCpuImage.Transformation.MirrorY
             };
 
+            int currentBufferSize = cpuImage.width * cpuImage.height * 4;
+
             if (cameraTexture == null || cameraTexture.width != cpuImage.width || cameraTexture.height != cpuImage.height)
             {
                 if (cameraTexture != null)
                     Destroy(cameraTexture);
 
                 cameraTexture = new Texture2D(cpuImage.width, cpuImage.height, TextureFormat.RGBA32, false);
+                bufferSize = currentBufferSize;
+                if (rawPixelBuffer.IsCreated)
+                    rawPixelBuffer.Dispose();
+                rawPixelBuffer = new NativeArray<byte>(bufferSize, Allocator.Persistent);
+            }
+            else if (bufferSize != currentBufferSize)
+            {
+                bufferSize = currentBufferSize;
+                if (rawPixelBuffer.IsCreated)
+                    rawPixelBuffer.Dispose();
+                rawPixelBuffer = new NativeArray<byte>(bufferSize, Allocator.Persistent);
             }
 
-            cpuImage.Convert(conversionParams, cameraTexture.GetRawTextureData<byte>());
+            cpuImage.Convert(conversionParams, rawPixelBuffer);
+            cameraTexture.LoadRawTextureData(rawPixelBuffer);
             cameraTexture.Apply();
 
-            // RawImage�� �ؽ�ó ������Ʈ
+            // 이미지 회전
+            cameraTexture = Rotate90(cameraTexture);
+
+            // RawImage의 텍스처 업데이트
             rawImage.texture = cameraTexture;
-            rawImage.rectTransform.localEulerAngles = new Vector3(0, 0, -90);
-            rawImage.rectTransform.sizeDelta = new Vector2(800, 800);
+            rawImage.rectTransform.sizeDelta = new Vector2(1000, 750);
+            rawImage.rectTransform.localEulerAngles = new Vector3(0, 0, 180); // 회전 유지
         }
     }
 }
