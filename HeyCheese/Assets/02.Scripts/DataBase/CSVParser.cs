a049b3e3b96c9e576ad81b34d930e9ce45d58e45
Ermion <ermion0357@gmail.com>
2025-05-04 07:42:36 +0900

    Feat: EpisodeMenu 정보 표시 및 MainStory 씬 이동
    - csv 파싱하여 EpisodeMenu에 버튼 생성 및 정보 표시
    - MainStory로 선택한 EpisodeID 전달할 수 있도록 PlayerPrefs 저장 및 씬 이동 연결

--- HeyCheese/Assets/02.Scripts/DataBase/CSVParser.cs
+++ HeyCheese/Assets/02.Scripts/DataBase/CSVParser.cs
@@ -6,11 +6,12 @@ public class CSVParser
 {
     // CSV 파일
     private TextAsset eduMenuCSV = Resources.Load<TextAsset>("Datas/EduMenu");
-    //private TextAsset eduStoryCSV = Resources.Load<TextAsset>("Datas/EduStory");
-    //private TextAsset mainStoryCSV = Resources.Load<TextAsset>("Datas/MainStory");
-
+    private TextAsset storyMenuCSV = Resources.Load<TextAsset>("Datas/StoryMenu");
+    private TextAsset mainStoryCSV = Resources.Load<TextAsset>("Datas/MainStory");
+    
     // Data Structure
     private Dictionary<string, List<EduMenu>> eduMenus = new Dictionary<string, List<EduMenu>>();
+    private Dictionary<string, StoryMenu> storyMenus = new Dictionary<string, StoryMenu>();
 
     // EduMenu
     // emotionID 하위에 situationID들이 리스트로 저장되는 형태의 딕셔너리
@@ -19,7 +20,7 @@ public class CSVParser
     {
         string[] lines = eduMenuCSV.text.Split('\n');
 
-        for(int i = 1; i < lines.Length; i++) // 0은 헤더라 1부터 시작
+        for (int i = 1; i < lines.Length; i++) // 0은 헤더라 1부터 시작
         {
             if (string.IsNullOrWhiteSpace(lines[i])) continue;
 
@@ -42,4 +43,61 @@ public class CSVParser
 
         return eduMenus;
     }
+
+    // StoryMenu
+    // episodeID 하위에 chapterTitle들이 리스트로 저장되는 형태의 딕셔너리
+    // 키: episodeID, 값: StoryMenu 객체 -> {"episdoeID": StoryMenu객체, ...}
+    // 에피소드 클릭 시 에피소드 번호에 따라 메인스토리 진행될 수 있도록 해야 됨
+    public Dictionary<string, StoryMenu> ParseStoryMenus()
+    {
+        string[] lines = storyMenuCSV.text.Split('\n');
+
+        for (int i = 1; i < lines.Length; i++) // 0은 헤더라 1부터 시작
+        {
+            if (string.IsNullOrWhiteSpace(lines[i])) continue;
+
+            string[] fields = lines[i].Split(',');
+
+            string episodeID = fields[0].Trim();
+            string chapterTitle = fields[1].Trim();
+            string imagePath = fields[2].Trim();
+
+            StoryMenu storyMenu = new StoryMenu(episodeID, chapterTitle, imagePath);
+
+            if (!storyMenus.ContainsKey(episodeID))
+            {
+                storyMenus[episodeID] = storyMenu;
+            }
+        }
+
+        return storyMenus;
+    }
+
+    // MainStory
+    //public Dictionary<string, List<EduMenu>> ParseMainStories()
+    //{
+    //    string[] lines = mainStoryCSV.text.Split('\n');
+
+    //    for (int i = 1; i < lines.Length; i++) // 0은 헤더라 1부터 시작
+    //    {
+    //        if (string.IsNullOrWhiteSpace(lines[i])) continue;
+
+    //        string[] fields = lines[i].Split(',');
+
+    //        string episodeID = fields[0].Trim();
+    //        string chapterTitle = fields[1].Trim();
+    //        string imagePath = fields[2].Trim();
+
+    //        StoryMenu storyMenu = new StoryMenu(episodeID, chapterTitle, imagePath);
+
+    //        if (!storyMenus.ContainsKey(episodeID))
+    //        {
+    //            storyMenus[episodeID] = new List<StoryMenu>();
+    //        }
+
+    //        storyMenus[episodeID].Add(storyMenu); // 리스트에 추가
+    //    }
+
+    //    return eduMenus;
+    //}
 }
