a8fd41bb5dfab8bfb5ec36327b9027782be0922d
limce106 <limce106@naver.com>
2025-05-03 17:28:32 +0900

    Feat: Cheese One Cut Buttons
    - 치즈 한 컷 버튼 연결
    
    - 치즈 한 컷 내 버튼 기능을 모아 놓은 CheeseOneCutButtons 스크립트 추가
    - 필터, 프레임 패널이 켜진 상태에서 패널 바깥쪽을 클릭하면 패널이 닫히도록 함
    
    - FrameApplier, ARCameraToRawImage 등 일부 스크립트의 역할이 폴더명과 맞지 않아, FaceRecog 폴더명을 CheeseOneCut으로 변경

---
+++ HeyCheese/Assets/02.Scripts/CheeseOneCut/CheeseOneCutButtons.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheeseOneCutButtons : MonoBehaviour
+{
+    public GameObject framePanel;
+    public GameObject filterPanel;
+
+    public Button cancelButton;
+    public Button cameraButton;
+
+    private void Start()
+    {
+        //cameraButton.onClick.AddListener(() => StartCoroutine(CaptureAndSave("story")));
+    }
+
+    public void OnClick_Gallery()
+    {
+        // 갤러리로 이동
+    }
+
+    public void OnClick_Frame()
+    {
+        framePanel.SetActive(true);
+        cancelButton.enabled = true;
+    }
+
+    public void OnClick_Filter()
+    {
+        filterPanel.SetActive(true);
+        cancelButton.enabled = true;
+    }
+
+    public void OnClick_Cancel()
+    {
+        if(framePanel.activeSelf)
+        {
+            framePanel.SetActive(false);
+        }
+        else if(filterPanel.activeSelf)
+        {
+            filterPanel.SetActive(false);
+        }
+
+        cancelButton.enabled = false;
+    }
+
+
+}
