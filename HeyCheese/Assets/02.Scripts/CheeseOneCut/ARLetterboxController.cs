1d35c67179e20755fef12079fda2b8a349e1c645
limce106 <limce106@naver.com>
2025-05-21 00:15:29 +0900

    Feat: Fix AR Camera Ratio By Adding Left/Right Letter Box
    - 세로보다 가로가 더 긴 핸드폰(Z폴드 등)도 4:3 비율로 고정할 수 있도록 구현
    
    - 좌우 레터박스도 추가하여, 핸드폰의 가로와 세로 길이 중 어떤 것이 더 긴지 판별하여 가로가 더 길면 좌우 레터박스 사용, 세로가 더 길면 상하 레터박스 사용
    
    - UI명 수정

--- HeyCheese/Assets/02.Scripts/CheeseOneCut/ARLetterboxController.cs
+++ HeyCheese/Assets/02.Scripts/CheeseOneCut/ARLetterboxController.cs
@@ -7,21 +7,61 @@ public class ARLetterboxController : MonoBehaviour
 {
     public Image topLetterbox;
     public Image bottomLetterbox;
+    public Image leftLetterbox;
+    public Image rightLetterbox;
     void Start()
+    {
+        ApplyLetterbox();
+    }
+
+    void ApplyLetterbox()
     {
         float targetAspect = 3f / 4f;
         float screenWidth = Screen.width;
         float screenHeight = Screen.height;
+        float screenAspect = screenWidth / screenHeight;
 
-        // 꽉 채운 화면 너비를 기준으로 높이 계산
-        float targetHeight = screenWidth / targetAspect;
-        float totalLetterboxHeight = screenHeight - targetHeight;
-        float letterboxHeight = totalLetterboxHeight / 2f;
+        if (screenAspect > targetAspect)
+        {
+            // 화면이 더 가로로 길면 좌우에 레터박스(pillarbox) 필요
+            float targetWidth = screenHeight * targetAspect;
+            float sideBoxWidth = (screenWidth - targetWidth) / 2f;
 
-        topLetterbox.rectTransform.sizeDelta = new Vector2(screenWidth, letterboxHeight);
-        bottomLetterbox.rectTransform.sizeDelta = new Vector2(screenWidth, letterboxHeight);
+            leftLetterbox.rectTransform.sizeDelta = new Vector2(sideBoxWidth, screenHeight);
+            rightLetterbox.rectTransform.sizeDelta = new Vector2(sideBoxWidth, screenHeight);
 
-        topLetterbox.rectTransform.anchoredPosition = new Vector2(0, screenHeight / 2f - letterboxHeight / 2f);
-        bottomLetterbox.rectTransform.anchoredPosition = new Vector2(0, -(screenHeight / 2f - letterboxHeight / 2f));
+            leftLetterbox.rectTransform.anchoredPosition = new Vector2(-((screenWidth / 2f) - (sideBoxWidth / 2f)), 0);
+            rightLetterbox.rectTransform.anchoredPosition = new Vector2((screenWidth / 2f) - (sideBoxWidth / 2f), 0);
+
+            leftLetterbox.gameObject.SetActive(true);
+            rightLetterbox.gameObject.SetActive(true);
+            topLetterbox.gameObject.SetActive(false);
+            bottomLetterbox.gameObject.SetActive(false);
+        }
+        else if (screenAspect < targetAspect)
+        {
+            // 화면이 더 세로로 길면 위아래에 레터 박스 필요
+            float targetHeight = screenWidth / targetAspect;
+            float boxHeight = (screenHeight - targetHeight) / 2f;
+
+            topLetterbox.rectTransform.sizeDelta = new Vector2(screenWidth, boxHeight);
+            bottomLetterbox.rectTransform.sizeDelta = new Vector2(screenWidth, boxHeight);
+
+            topLetterbox.rectTransform.anchoredPosition = new Vector2(0, (screenHeight / 2f) - (boxHeight / 2f));
+            bottomLetterbox.rectTransform.anchoredPosition = new Vector2(0, -((screenHeight / 2f) - (boxHeight / 2f)));
+
+            topLetterbox.gameObject.SetActive(true);
+            bottomLetterbox.gameObject.SetActive(true);
+            leftLetterbox.gameObject.SetActive(false);
+            rightLetterbox.gameObject.SetActive(false);
+        }
+        else
+        {
+            // 이미 화면 비율이 4:3이면 아무것도 필요 없음
+            topLetterbox.gameObject.SetActive(false);
+            bottomLetterbox.gameObject.SetActive(false);
+            leftLetterbox.gameObject.SetActive(false);
+            rightLetterbox.gameObject.SetActive(false);
+        }
     }
 }
