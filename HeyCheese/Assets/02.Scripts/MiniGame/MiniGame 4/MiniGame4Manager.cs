030a4e9c0802299c09ecf89deba39936579fc44c
Jieun <jieundino@naver.com>
2025-05-22 02:57:05 +0900

    chore: MiniGame4
    # 미니게임 4 어른 몰래 춤추기
    - 진행바의 핸들 이미지 미러볼 아이콘으로 변경 및 fill 색상 수정
    - 터치 안 했을 때는 핸들 이미지 미러볼이 어둡게 처리되도록 수정
    - 게임 시작하고 40%로 랜덤하게 어른이 등장하게 함.
    - 노크 소리 재생으로 전조 증상을 청각적으로 들을 수 있게 함.
    - 어른이 방 안에 들어왔을 때는 자는 척 유지.
    - 어른이 방 안에 들어왔을 때 터치하여 춤추게 되면 바로 Fail되어서 Restart 패널 활성화
    - Restart 버튼 누를 시, 미니게임4 변수들 초기화 후 게임 스타트 진행

--- HeyCheese/Assets/02.Scripts/MiniGame/MiniGame 4/MiniGame4Manager.cs
+++ HeyCheese/Assets/02.Scripts/MiniGame/MiniGame 4/MiniGame4Manager.cs
@@ -11,27 +11,53 @@ public class MiniGame4Manager : MonoBehaviour
 
     [SerializeField] private Slider progressSlider;
 
+    [Header("진행바 핸들 이미지")]
+    [SerializeField] private Image barHandleImage;
+    // 터치 안 하면 878787 헥사 코드로 변경 
+
     public float fillDuration = 15f; // 15초에 다 채워짐
     public float decayRate = 0.1f;   // 초당 감소 비율
 
-    public bool isRestart = false;
+    [Header("Adult 이미지 | Sprite 리스트 0:LightOn, 1:LightOff")]
+    [SerializeField] private Image AdultImage;
+    [SerializeField] private List<Sprite> AdultSprites;
 
-    private bool isTouching;
-    private bool isCleared = false;
+    // 어른 랜덤으로 등장(효과음 재생 이후 2초 후에 등장)
+    [Header("Adult 랜덤으로 등장할 확률")]
+    [SerializeField] private int apprearanceProbability = 40;
+    public bool isAdultComing = false;
 
     [Header("패널들")]
     [SerializeField] private GameObject GuidePanel;
     [SerializeField] private GameObject ClearPanel;
 
+    private bool isTouching;
+    private bool isCleared = false;
+
     private bool isStart;
+    public bool isRestart = false;
 
     [SerializeField] private GameObject RestartPanel;
+
+    // 임시 효과음 재생
+    // 사운드 플레이어 만들면 그쪽 이용해서 사용할 예정
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource SFXSoundPlayer;
+
+    [Header("AudioClip")]
+    [SerializeField] private AudioClip[] SFXSoundClips;
+
+    private const int Sound_Knocking = 0,
+        Sound_OpenDoor = 1,
+        Sound_CloseDoor = 2;
+
 
     private void Awake()
     {
         Time.timeScale = 0f;
 
         isStart = false;
+        isAdultComing = false;
 
         //animators = new Animator[characterObjects.Length];
         for (int i = 0; i < characterObjects.Length; i++)
@@ -43,6 +69,7 @@ public class MiniGame4Manager : MonoBehaviour
         GuidePanel.SetActive(true);
         ClearPanel.SetActive(false);
         SetRestartActive(false);
+        SetAdultImage(false);
     }
 
     void Update()
@@ -59,13 +86,41 @@ public class MiniGame4Manager : MonoBehaviour
         UpdateProgressSlider();
         // Clear 조건 달성 체크
         CheckClearCondition();
+
+        // 게임 진행 중일 때만 체크
+        if (isStart && !isCleared && isAdultComing && isTouching)
+        {
+            TriggerRestart();
+        }
+    }
+
+    private void TriggerRestart()
+    {
+        if (isRestart) return; // 이미 처리된 경우 중복 방지
+
+        isRestart = true;
+
+        SetRestartActive(true);
+
+        StopAllCoroutines();
     }
 
     public void StartGame()
     {
+        if (isRestart)
+        {
+            StopAllCoroutines();
+            InitMiniGame4();
+        }
+
         GuidePanel.SetActive(false);
+        SetRestartActive(false);
+
         Time.timeScale = 1f;
         isStart = true;
+        isRestart = false;
+
+        StartCoroutine(CheckAdultAppearanceLoop());
     }
 
     // 터치 감지 메소드
@@ -93,6 +148,9 @@ public class MiniGame4Manager : MonoBehaviour
             ActivateCharacters(false);
             Background_Sleeping.SetActive(true);
         }
+
+        if (isAdultComing)
+            SetAdultImage(true);
     }
 
     private void UpdateProgressSlider()
@@ -101,16 +159,37 @@ public class MiniGame4Manager : MonoBehaviour
         if (isTouching)
         {
             progressSlider.value += Time.deltaTime / fillDuration;
+
+            // 핸들 이미지도 밝게 처리
+            barHandleImage.color = HexColor("#FFFFFF");
         }
         // 터치 안 할 때, 슬라이더 서서히 감소함
         else
         {
             progressSlider.value -= Time.deltaTime * decayRate;
+
+            // 슬라이더바의 핸들 이미지도 어둡게 처리
+            barHandleImage.color = HexColor("#878787");
+
         }
 
         // value가 0~1 사이에서만 유지되도록 제한
         progressSlider.value = Mathf.Clamp01(progressSlider.value);
     }
+
+    // 헥사값 컬러 반환( 코드 순서 : RGBA )
+    private Color HexColor(string hexCode)
+    {
+        Color color;
+        if (ColorUtility.TryParseHtmlString(hexCode, out color))
+        {
+            return color;
+        }
+
+        Debug.LogError("[UnityExtension::HexColor]invalid hex code - " + hexCode);
+        return Color.white;
+    }
+
 
     private void CheckClearCondition()
     {
@@ -142,11 +221,95 @@ public class MiniGame4Manager : MonoBehaviour
 
     private void SetRestartActive(bool isShow)
     {
-        if (!isRestart)
-            return;
-
         RestartPanel.SetActive(isShow);
         Time.timeScale = 0f;
         isStart = false;
+    }
+
+    private void InitMiniGame4()
+    {
+        progressSlider.value = 0f;
+
+        GuidePanel.SetActive(false);
+        ClearPanel.SetActive(false);
+        isAdultComing = false;
+        SetAdultImage(false);
+    }
+
+
+    private IEnumerator CheckAdultAppearanceLoop()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(AppearRandomly());
+
+            // 다음 시도를 일정 시간 후에 다시
+            yield return new WaitForSeconds(Random.Range(3f, 10f));
+        }
+    }
+
+    private IEnumerator AppearRandomly()
+    {
+        int randomNumber = Random.Range(0, 100);
+        float waitingTime = 3f;
+
+        if (randomNumber < apprearanceProbability)
+        {
+            // 노크 소리 재생
+            SFXSoundPlay(Sound_Knocking);
+
+            yield return new WaitForSeconds(waitingTime);
+            // 문 여는 효과음 재생
+            SFXSoundPlay(Sound_OpenDoor);
+
+            // 효과음 다 재생된 후 등장
+            yield return new WaitWhile(()=>SFXSoundPlayer.isPlaying);
+
+            isAdultComing = true;
+            SetAdultImage(true);
+
+            // 일정 시간 후 사라지기
+            yield return new WaitForSeconds(waitingTime);
+
+            // 문 닫는 효과음 재생
+            SFXSoundPlay(Sound_CloseDoor);
+
+            // 효과음 다 재생된 후 사라짐
+            yield return new WaitWhile(() => SFXSoundPlayer.isPlaying);
+
+            isAdultComing = false;
+            SetAdultImage(false);
+        }
+    }
+
+
+
+    private void SetAdultImage(bool isActive)
+    {
+        if (isActive&& isAdultComing)
+        {
+            AdultImage.gameObject.SetActive(true);
+
+            // 어른 왔을 때 터치하고 있으면 불켜져 있는 상태 스프라이트인 0번째로 변경
+            AdultImage.sprite = AdultSprites[
+                (isTouching==true ? 0 : 1)
+                ];
+        }
+        else
+        {
+            AdultImage.gameObject.SetActive(false);
+        }
+
+    }
+
+
+    // 사운드 플레이
+    private void SFXSoundPlay(int clipIndex)
+    {
+        // 재생할 효과음 변경
+        SFXSoundPlayer.clip = SFXSoundClips[clipIndex];
+
+        // 음악 재생
+        SFXSoundPlayer.Play();
     }
 }
