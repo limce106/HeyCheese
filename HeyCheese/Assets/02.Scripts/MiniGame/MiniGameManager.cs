960923fa3f643c812f506bd4e75fa81e38b60a17
Jieun <jieundino@naver.com>
2025-05-31 23:14:32 +0900

    Fix: 미니게임4 끝나고 메인 스토리 씬 이동 안 되는 오류
    - 미니게임4에서 클리어 시, 시간 정지를 해뒀기에
    IEnumerator BackToMainStory()의 대기 방식을 yield return new WaitForSecondsRealtime(2f); 으로 현실 시간 2초 대기 후 실행하도록 함.

--- HeyCheese/Assets/02.Scripts/MiniGame/MiniGameManager.cs
+++ HeyCheese/Assets/02.Scripts/MiniGame/MiniGameManager.cs
@@ -18,7 +18,8 @@ abstract public class MiniGameManager : MonoBehaviour
 
     public IEnumerator BackToMainStory()
     {
-        yield return new WaitForSeconds(2f);
+        Debug.Log("2초 후 메인 스토리로 이동.");
+        yield return new WaitForSecondsRealtime(2f); // ← 시간 정지 무시하고 2초 기다림
 
         MainStoryGameManager.MainStoryGM.NextStep();
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainStory");
