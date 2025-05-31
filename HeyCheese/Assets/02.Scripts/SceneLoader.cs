7e2503d9e9c8f5567e68cd6cbe9913086cb74dc0
Jieun <jieundino@naver.com>
2025-05-31 23:21:46 +0900

    Feat: update SceneLoader
    - 메인 메뉴로 씬 이동 시, 메인 메뉴 브금 재생하는 코드 추가.

--- HeyCheese/Assets/02.Scripts/SceneLoader.cs
+++ HeyCheese/Assets/02.Scripts/SceneLoader.cs
@@ -8,5 +8,10 @@ public class SceneLoader : MonoBehaviour
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        // 메인 메뉴로 이동 시, 메인 메뉴 브금 재생
+        if (sceneName == "MainMenu")
+        {
+            SoundPlayer.Instance.ChangeBGM((int)SoundPlayer.BGM.DEFAULT_BGM);
+        }
     }
 }
